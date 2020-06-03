1ba60aca12959374ba2d1bf01c8e074d45bf1496
Bruno Carneiro <bruno.carneiro312@gmail.com>
2020-06-03 20:08:35 -0300

    distribuição de sequencia

--- src/views/Sequencias.vue
+++ src/views/Sequencias.vue
@@ -20,8 +20,12 @@
                <div class="sm:w-1/2 mb-10 px-4">
                   <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">Distribuição Avulsa</h2>
                   <p class="leading-relaxed text-base">Distribui as sequências que não tem cliente de forma avulsa.</p>
-                  <input style="max-width: 200px" class="mt-3 w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-green-500 text-base px-4 py-2" placeholder="Jogos por cliente" type="number">
-                  <button class="flex mx-auto mt-6 text-white bg-green-500 border-0 py-2 px-5 focus:outline-none hover:bg-green-600 rounded">Distribuição avulsa</button>
+                  <input style="max-width: 200px"
+                         class="mt-3 w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-green-500 text-base px-4 py-2"
+                         placeholder="Jogos por cliente"
+                         v-model="qtdJogosPorPessoa"
+                         type="number">
+                  <button @click="distribuirAvulso" class="flex mx-auto mt-6 text-white bg-green-500 border-0 py-2 px-5 focus:outline-none hover:bg-green-600 rounded">Distribuição avulsa</button>
                </div>
             </div>
          </div>
@@ -69,8 +73,8 @@
                                     </tr>
                                  </thead>
                                  <tbody>
-                                    <tr v-for="jogo in p.jogos" :key="jogo.codigoJogo">
-                                       <td>{{ jogo.sequencia }}</td>
+                                    <tr v-for="(jogo, index) in p.jogos" :key="index">
+                                       <td v-if="jogo">{{ jogo.sequencia }}</td>
                                     </tr>
                                  </tbody>
                               </table>
@@ -102,6 +106,8 @@
         name: "Sequencias",
         data: function() {
             return {
+                qtdJogosPorPessoa: undefined,
+                mode: undefined,
                 clientes: [],
                 jogosSemDono: [],
                 previa: [],
@@ -155,6 +161,7 @@
                  })
             },
             async distribuirIgualmente() {
+                this.mode = 1;
                 await this.listarClientes();
                 await this.listarJogosSemDono();
 
@@ -163,7 +170,6 @@
                 let qtdJogosPorCliente = qtdJogosSemDono / qtdClientes;
 
                 if (qtdClientes && qtdJogosSemDono) {
-                    console.log(qtdJogosPorCliente);
 
                     this.clientes.forEach((c) => {
                         this.request.clientesDTORequest.push({
@@ -195,29 +201,97 @@
                 }
             },
 
-            confirmarDistribuicao() {
-                if (this.jogosSemDono.length && this.clientes.length) {
-                    axios.post("http://localhost:8080/loteriasvip/api/v1/jogos/distribuirJogosSemDono", undefined,{
-                        headers: {
-                            'Authorization': 'Bearer ' + localStorage.getItem('tokenData')
-                        }
-                    })
-                        .then((response) => {
-                            console.log(response.data);
-                            this.$toast.open({
-                                message: 'Distribuição realizada com sucesso!',
-                                type: 'success',
-                                position: 'top-right'
-                            })
-                        })
-                        .catch((error) => {
-                            console.log(error);
-                            this.$toast.open({
-                                message: 'Erro ao realizar a distribuição.',
-                                type: 'success',
-                                position: 'top-right'
-                            })
+            async distribuirAvulso() {
+                this.mode = 2;
+                await this.listarClientes();
+                await this.listarJogosSemDono();
+
+                let qtdClientes = this.clientes.length;
+                let qtdJogosSemDono = this.jogosSemDono.length;
+                let qtdJogosPorCliente = this.qtdJogosPorPessoa;
+
+                if (qtdClientes && qtdJogosSemDono) {
+                    this.clientes.forEach((c) => {
+                        this.request.clientesDTORequest.push({
+                            "email": c.email
                         });
+                    });
+
+                    this.jogosSemDono.forEach((j) => {
+                        this.request.jogosDTORequest.push({
+                            "codigoJogo": j.codigoJogo
+                        });
+                    });
+
+                    let countJogoVigente = 0;
+
+                    for (let i = 0; i < qtdClientes; i++) {
+                        let cliente = {
+                            nome: this.clientes[i].nome,
+                            email: this.clientes[i].email,
+                            jogos: []
+                        };
+
+                        for (let x = 0; x < qtdJogosPorCliente; x++) {
+                            cliente.jogos.push(this.jogosSemDono[countJogoVigente++]);
+                        }
+
+                        this.previa.push(cliente);
+                    }
+                }
+            },
+
+            confirmarDistribuicao() {
+                if (this.mode === 1) {
+
+                   if (this.jogosSemDono.length && this.clientes.length) {
+                       axios.post("http://localhost:8080/loteriasvip/api/v1/jogos/distribuirJogosSemDono", undefined,{
+                           headers: {
+                               'Authorization': 'Bearer ' + localStorage.getItem('tokenData')
+                           }
+                       })
+                           .then((response) => {
+                               console.log(response.data);
+                               this.$toast.open({
+                                   message: 'Distribuição realizada com sucesso!',
+                                   type: 'success',
+                                   position: 'top-right'
+                               })
+                           })
+                           .catch((error) => {
+                               console.log(error);
+                               this.$toast.open({
+                                   message: 'Erro ao realizar a distribuição.',
+                                   type: 'error',
+                                   position: 'top-right'
+                               })
+                           });
+                   }
+                }
+                if (this.mode === 2) {
+                    if (this.jogosSemDono.length && this.clientes.length) {
+                        axios.post("http://localhost:8080/loteriasvip/api/v1/jogos/distribuirJogosSemDonoAvulso/" + this.qtdJogosPorPessoa, undefined,{
+                            headers: {
+                                'Authorization': 'Bearer ' + localStorage.getItem('tokenData')
+                            }
+                        })
+                         .then((response) => {
+                             console.log(response.data);
+                             this.$toast.open({
+                                 message: 'Distribuição realizada com sucesso!',
+                                 type: 'success',
+                                 position: 'top-right'
+                             })
+                         })
+                         .catch((error) => {
+                             console.log(error);
+                             this.$toast.open({
+                                 message: 'Erro ao realizar a distribuição.',
+                                 type: 'error',
+                                 position: 'top-right'
+                             })
+                         });
+                    }
                 }
             }
         }
